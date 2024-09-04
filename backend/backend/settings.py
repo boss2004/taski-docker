9fc0645caeac9350d2110dada5941565a7645c3c
Игорь Горбунов <boss.igor14204@mail.ru>
2024-09-04 15:00:11 +0300

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# Указываем корневую директорию для сборки статических файлов;
+# в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
